80a627c3d82e3fc9ea82395e6c532ad26e521670
Greg McClement <greg.mcclement@gmail.com>
2021-02-27 08:27:39 -0800

    start on fw for km's

--- kms/common/time.js
+++ kms/common/time.js
@@ -1,24 +1,44 @@
 const client = require('entodicton/client')
 const Config = require('entodicton/src/config')
 
+const pad = (v, l) => {
+  const s = String(v)
+  const n = l - s.length
+  return "0".repeat(n) + s
+}
+
 let config = {
   operators: [
-    "((([what]) [timeConcept|time]) [equal|is] ([it]))",
+    "((([what]) [timeConcept|time]) [equals|is] ([it]))",
+    "([use] ((<count> ([timeUnit])) [timeFormat|format]))"
+    //"(([what]) [equals] ([the] ([timeConcept])))",
+    //"what is the time in 24 hour format"
     //"what time is it in Paris"
     //"what time is it in GMT"
     // what is the time
+    // how many hours are in a day
   ],
   bridges: [
     { "id": "what", "level": 0, "bridge": "{ ...next(operator), isQuery: true }" },
-    { "id": "equal", "level": 0, "bridge": "{ ...next(operator), equals: [before, after] }" },
+    { "id": "equals", "level": 0, "bridge": "{ ...next(operator), equals: [before, after] }" },
     { "id": "it", "level": 0, "bridge": "{ ...next(operator), pullFromContext: true }" },
     { "id": "timeConcept", "level": 0, "bridge": "{ ...next(operator) }" },
+
+    { "id": "timeFormat", "level": 0, "bridge": "{ ...before[0], ...next(operator) }" },
+    { "id": "count", "level": 0, "bridge": "{ ...after, count: operator[0].value }" },
+    { "id": "timeUnit", "level": 0, "bridge": "{ ...next(operator) }" },
+    { "id": "use", "level": 0, "bridge": "{ ...next(operator), format: after[0] }" },
   ],
+  floaters: ['isQuery'],
   debug: true,
   priorities: [
   ],
   "version": '3',
   "words": {
+    " ([0-9]+)": [{"id": "count", "initial": "{ value: int(group[0]) }" }],
+    " hours?": [{"id": "timeUnit", "initial": "{ units: 'hour' }" }],
+    " minutes?": [{"id": "timeUnit", "initial": "{ units: 'hour' }" }],
+    " seconds?": [{"id": "timeUnit", "initial": "{ units: 'seconds' }" }],
     //"spock": [{"id": "crewMember", 'initial': { 'id': 'spock' } }],
     /*
     " ([0-9]+)": [{"id": "count", "initial": "{ value: int(group[0]) }" }],
@@ -31,71 +51,107 @@ let config = {
   },
 
   generators: [
-    [ ({context}) => context.marker == 'crewMember', ({g, context}) => `${g(context.word)}` ],
-    [ ({context}) => context.marker == 'arm' && context.paraphrase != 'self', ({g, context}) => `${g(context.word)} ${g(context.weapon)}` ],
-    [ ({context}) => context.marker == 'arm' && context.paraphrase == 'self', ({g, context}) => `I am arming ${g(context.weapon)}` ],
-    [ ({context}) => context.marker == 'torpedoConcept' && context.type == 'photon', ({g, context}) => `the ${context.type} ${g(context.word)}` ],
-    [ ({context}) => context.marker == 'showStatus' && context.hasAnswer, ({g, context}) => `Ship status is: ${context.status}` ],
+    [ ({context}) => context.marker == 'equals', ({g, context}) => `${g(context.equals[0])} is ${g(context.equals[1])}` ],
+    [ ({context}) => context.marker == 'timeConcept' && context.value && context.format == 12, ({g, context}) => {
+          let hh = context.value.getHours();
+          let ampm = 'am'
+          if (hh > 12) {
+            hh -= 12;
+            ampm = 'pm'
+          }
+          let ss = context.value.getSeconds()
+          ss = pad(ss, 2)
+          return `${hh}:${ss} ${ampm}` 
+        }],
+    [ ({context}) => context.marker == 'timeConcept' && context.value && context.format == 24, ({g, context}) => 
+        `${context.value.getHours()}:${context.value.getMinutes()}` ],
+    [ ({context}) => context.marker == 'timeConcept' && !context.value, ({g, context}) => `the time` ],
+    [ ({context}) => context.marker == 'use' && !context.value, ({g, context}) => `the time` ],
+    [ ({context}) => context.marker == 'response', ({g, context}) => context.text ],
   ],
 
   semantics: [
-    [({global, context}) => context.marker == 'action', async ({global, context}) => {
-      if (context.actor.pullFromContext) {
-        actor = global.mentions[0]
-        console.log('wwwwwwwwwwwwwwwwwwwww', global.characters[actor].toDo)
-        debugger;
-        const { context, generated } = client.processContext(global.characters[actor].toDo[0], { semantics: config.get('semantics'), generators: config.get('generators'), objects: config.get('objects') } )
-        console.log('context', context)
-        console.log('generated', generated)
-        //result = await client.process(url, key, config) 
-        //console.log('rrrrrrrrrrrrrrrrrrrrrrrrr', result)
+    [({global, context}) => context.marker == 'equals' && context.isQuery, async ({global, context}) => {
+      context.isResponse = true
+      delete context.isQuery
+      delete context.equals[0].isQuery
+      context.equals[1] = {
+        marker: 'timeConcept',
+        value: new Date(),
+        format: global.format
       }
-     }],
-    [({global, context}) => context.marker == 'crewMember', ({global, context}) => {
-      global.mentions.push(context.id)
-     }],
-    [({global, context}) => context.marker == 'arm', ({global, context}) => {
-      if (global.mentions.length > 0) {
-        // character does it
-        global.characters[global.mentions[0]].toDo.push(context)
-      } else {
-        // computer does it right away
-        global.enterprise.torpedoes.armed = true
-      }
-     }],
-    [({global, context}) => context.marker == 'showStatus' && context.area == 'weapons', ({global, context}) => {
-      context.hasAnswer = true
-      const ps = global.enterprise.phasers.armed ? 'armed' : 'not armed'
-      const ts = global.enterprise.torpedoes.armed ? 'armed' : 'not armed'
-      context.status = `Phasers: ${ps} Torpedoes: ${ts}`
-     }],
+    }],
+    [({global, context}) => context.marker == 'use' && context.format && (context.format.count == 12 || context.format.count == 24), async ({global, context}) => {
+      global.format = context.format.count
+    }],
+    [({global, context}) => context.marker == 'use' && context.format && (context.format.count != 12 && context.format.count != 24), async ({global, context}) => {
+      context.marker = 'response'
+      context.text = 'The hour format is 12 hour or 24 hour'
+    }],
   ],
 };
 
 url = 'http://Deplo-Entod-17J794A370LM3-1965629916.ca-central-1.elb.amazonaws.com'
 key = 'f4a879cd-6ff7-4f14-91db-17a11ba77103'
 
-config.utterances = ['what time is it']
+//config.utterances = ['what time is it']
+//config.utterances = ['use 24 hour format what time is it use 12 hour format what time is it']
+config.utterances = ['use 36 hour format']
 console.log(`Running the input: ${config.utterances}`);
+config.objects = {
+  format: 12  // or 24
+};
 config = new Config(config)
 
-client.process(url, key, config)
-  .then( async (responses) => {
-    if (responses.errors) {
-      console.log('Errors')
-      responses.errors.forEach( (error) => console.log(`    ${error}`) )
+const knowledgeModule = ({url, key, config, test, debug, module, stopAtFirstFailure = true} = {}) => {
+  if ((typeof process) !== 'undefined') {
+    const hasTestFlag = () => {
+      return process.argv[process.argv.length-1] == 'test'
     }
-    console.log('This is the global objects from running semantics:\n', config.objects)
-    if (responses.logs) {
-      console.log('Logs')
-      responses.logs.forEach( (log) => console.log(`    ${log}`) )
+    if (hasTestFlag()) {
+      if (typeof test == 'string') {
+        client.runTests(url, key, config, test, { stopAtFirstError: true }).then( (failures) => {
+          console.log("failures", JSON.stringify(failures, null, 2))
+        })
+      } else {
+        test()
+      }
+    } else {
+      debug()
     }
-    console.log(responses.trace);
-    console.log(responses.generated);
-    console.log(JSON.stringify(responses.results, null, 2));
-  })
-  .catch( (error) => {
-    console.log(`Error ${config.get('utterances')}`);
-    console.log(error.error)
-    //console.log(error.trace)
-  })
+  } else {
+    module()
+  }
+}
+
+knowledgeModule( { 
+  url,
+  key,
+  config,
+  test: './time.test',
+  debug: () => {
+    client.process(url, key, config, { writeTests: true, testsFn: './time.test' })
+      .then( async (responses) => {
+        if (responses.errors) {
+          console.log('Errors')
+          responses.errors.forEach( (error) => console.log(`    ${error}`) )
+        }
+        console.log('This is the global objects from running semantics:\n', config.objects)
+        if (responses.logs) {
+          console.log('Logs')
+          responses.logs.forEach( (log) => console.log(`    ${log}`) )
+        }
+        console.log(responses.trace);
+        console.log('objects', JSON.stringify(config.get("objects"), null, 2))
+        console.log(JSON.stringify(responses.results, null, 2));
+        console.log(responses.generated);
+      })
+      .catch( (error) => {
+        console.log(`Error ${config.get('utterances')}`);
+        console.log(error.error)
+        console.log(error)
+      })
+  },
+  module: () => {
+  }
+})
