dba6cf6215a645ed0efde84718ab1ca5caf405bb
Greg McClement <greg.mcclement@gmail.com>
2021-03-06 14:02:23 -0800

    wip

--- kms/common/time.js
+++ kms/common/time.js
@@ -1,5 +1,6 @@
 const client = require('entodicton/client')
 const Config = require('entodicton/src/config')
+const Digraph = require('entodicton/src/digraph')
 
 const pad = (v, l) => {
   const s = String(v)
@@ -9,10 +10,10 @@ const pad = (v, l) => {
 
 let config = {
   operators: [
-    "((<what> ([timeConcept|time])) [equals|is] ([timeConcept]))",
+    "((<whatP|what> ([anyConcept])) [equals|is] ([anyConcept]))",
     "([use] ((<count> ([timeUnit])) [timeFormat|format]))",
-    "(([whatOne|what]) [equals] (<the> ([timeConcept])))",
-    "([anyConcept])",
+    //"(([whatOne|what]) [equals] (<the> ([timeConcept])))",
+    "(<the> ([timeConcept|time]))",
     //"what is the time in 24 hour format"
     //"what time is it in Paris"
     //"what time is it in GMT"
@@ -20,12 +21,12 @@ let config = {
     // how many hours are in a day
   ],
   bridges: [
-    { "id": "what", "level": 0, "bridge": "{ ...next(operator), isQuery: true }" },
+    { "id": "whatP", "level": 0, "bridge": "{ ...after, isQuery: true }" },
     { "id": "equals", "level": 0, "bridge": "{ ...next(operator), equals: [before, after] }" },
     { "id": "anyConcept", "level": 0, "bridge": "{ ...next(operator), pullFromContext: true }" },
     { "id": "timeConcept", "level": 0, "bridge": "{ ...next(operator) }" },
 
-    { "id": "whatOne", "level": 0, "bridge": "{ ...next(operator), isQuery: true }" },
+    //{ "id": "whatOne", "level": 0, "bridge": "{ ...next(operator), isQuery: true }" },
     { "id": "the", "level": 0, "bridge": "{ ...after, pullFromContext: true }" },
 
     { "id": "timeFormat", "level": 0, "bridge": "{ ...before[0], ...next(operator) }" },
@@ -81,15 +82,32 @@ let config = {
   ],
 
   semantics: [
-    [({global, context}) => context.marker == 'equals' && context.isQuery, async ({global, context}) => {
+    [({global, context}) => context.marker == 'equals' && context.isQuery, async ({global, context, s}) => {
       context.isResponse = true
       delete context.isQuery
       delete context.equals[0].isQuery
-      context.equals[1] = {
-        marker: 'timeConcept',
-        value: new Date(),
-        format: global.format
-      }
+      console.log('ssssssssssssssssssssssss', s)
+      // get the types
+      // share the types 
+      // run the semantics
+      markers = context.equals.map( (c) => c.marker )
+      mostSpecific = 'timeConcept'  // have a helper function
+      context.equals.map( (c) => c.marker = mostSpecific )
+      console.log('markers', markers);
+      context.equals.forEach( (c) => {
+        if (c.pullFromContext) {
+          console.log('runing c', c)
+          Object.assign(c, s(c))
+          console.log('new c', c)
+        }
+      })
+      console.log('context out', context);
+    }],
+    [({global, context}) => context.marker == 'timeConcept' && context.pullFromContext, async ({global, context}) => {
+      console.log("xxxxxxxxxxxxx in it");
+      context.value = new Date()
+      context.format = global.format
+      console.log('context outx xxxx', context)
     }],
     [({global, context}) => context.marker == 'use' && context.format && (context.format.count == 12 || context.format.count == 24), async ({global, context}) => {
       global.format = context.format.count
@@ -105,6 +123,7 @@ url = 'http://Deplo-Entod-17J794A370LM3-1965629916.ca-central-1.elb.amazonaws.co
 key = 'f4a879cd-6ff7-4f14-91db-17a11ba77103'
 
 config.utterances = ['what time is it']
+//config.utterances = ['the time']
 //config.utterances = ['use 24 hour format what time is it use 12 hour format what time is it']
 //config.utterances = ['what is the time']
 console.log(`Running the input: ${config.utterances}`);
@@ -154,7 +173,7 @@ knowledgeModule( {
         console.log(responses.trace);
         console.log('objects', JSON.stringify(config.get("objects"), null, 2))
         console.log(responses.generated);
-        console.log(responses.results);
+        console.log(JSON.stringify(responses.results, null, 2));
       })
       .catch( (error) => {
         console.log(`Error ${config.get('utterances')}`);
