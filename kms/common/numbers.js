60a40adab391f83a76f17883a0240812d7f1d7f5
Greg McClement <greg.mcclement@gmail.com>
2021-04-23 21:00:05 -0700

    wip

--- kms/common/numbers.js
+++ kms/common/numbers.js
@@ -12,14 +12,12 @@ let config = {
   version: '3',
   words: {
     " ([0-9]+)": [{"id": "number", "initial": "{ value: int(group[0]) }" }],
+    "one": [{"id": "number", "initial": "{ value: 1 }" }],
     "ten": [{"id": "number", "initial": "{ value: 10 }" }],
   },
 
   generators: [
     [ ({context}) => context.marker == 'number', ({context}) => `${context.value}` ],
-  ],
-
-  words: [
   ],
 
   semantics: [
