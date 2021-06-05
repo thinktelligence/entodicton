e6182971e87a9f02b3c1abf8ef2b3c27faae92be
Greg McClement <greg.mcclement@gmail.com>
2021-06-05 12:05:59 -0700

    wip

--- kms/common/store.js
+++ kms/common/store.js
@@ -1,5 +1,7 @@
 const entodicton = require('entodicton')
 const currencyKM = require('./currency.js')
+const helpKM = require('./help.js')
+const { table } = require('table')
 
 const testData = {
   types: [ 'pants', 'shorts' ],
@@ -27,10 +29,11 @@ const api = {
 let config = {
   operators: [
     //"(([type]) [([(<less> ([than]))] ([amount]))])",
-    "(([show] ([listingType|])) <([for] (<the> ([listings])))>)",
+    //"(([show] ([listingType|])) <([for] (<the> ([listings])))>)",
     "([list] (<the> (([product|products]))))",
     //"([list] ((<the> (([product|products]))) <(<that> ([cost] ([price])))>)) )"
     "(([product]) <(<that> ([cost] ([price])))>)",
+    "([answer] ([with] ([listingType|])))",
   ],
   bridges: [
     { "id": "product", "level": 0, "bridge": "{ ...next(operator) }" },
@@ -44,11 +47,16 @@ let config = {
     { "id": "cost", "level": 1, "bridge": "{ ...squish(operator), thing*: before[0] }" },
     { "id": "price", "level": 0, "bridge": "{ ...next(operator) }" },
 
+/*
     { "id": "show", "level": 0, "bridge": "{ ...next(operator), type: after[0] }" },
     { "id": "the", "level": 0, "bridge": "{ ...next(after[0]), instance: true }" },
     { "id": "for", "level": 0, "bridge": "{ ...next(operator), listable: after[0]}" },
     { "id": "for", "level": 1, "bridge": "{ ...next(before[0]), listable: operator.after[0]}" },
-    { "id": "tableType", "level": 0, "bridge": "{ ...next(operator) }" },
+*/
+
+    { "id": "listingType", "level": 0, "bridge": "{ ...next(operator) }" },
+    { "id": "with", "level": 0, "bridge": "{ ...next(operator), type: after[0].value }" },
+    { "id": "answer", "level": 0, "bridge": "{ ...next(operator), type: after[0].type }" },
   ],
   hierarchy: [
   ],
@@ -61,21 +69,49 @@ let config = {
   ],
   "version": '3',
   "words": {
-    "tables": [{"id": "listingType", "initial": "{ marker: tableType, value: 'tables' }" }],
-    "sentences": [{"id": "listingType", "initial": "{ marker: tableType, value: 'sentences' }" }],
+    "tables": [{"id": "listingType", "initial": "{ value: 'tables' }" }],
+    "sentences": [{"id": "listingType", "initial": "{ value: 'sentences' }" }],
     //" ([0-9]+)": [{"id": "amount", "initial": "{ value: int(group[0]) }" }],
   },
 
   generators: [
     [ ({context}) => context.marker == 'product' && !context.isInstance, ({context}) => `the ${context.word}` ],
     [ ({context}) => context.marker == 'list' && !context.isResponse, ({g, context}) => `list ${g(context.what)}` ],
-    [ ({context}) => context.marker == 'list' && context.isResponse, ({g, gs, context}) => `${g(context.what)} are ${gs(context.listing, ' ', ' and ')}` ],
+    [ 
+      ({context, objects}) => objects.listing.type == 'sentences' && context.marker == 'list' && context.isResponse, 
+      ({g, gs, context}) => `${g(context.what)} are ${gs(context.listing, ' ', ' and ')}` 
+    ],
+    [ 
+      ({context, objects}) => objects.listing.type == 'tables' && context.marker == 'list' && context.isResponse, 
+      ({g, gs, objects, context}) => {
+        let report = '';
+        const products = context.listing
+        const columns = objects.listing.columns
+        const data = products.map( (product) => {
+                const row = []
+                for (property of columns) {
+                  row.push(product[property])
+                }
+                return row
+               });
+        report += table([columns].concat(data))
+        return report
+      }
+    ],
+    [ 
+      ({context}) => context.marker == 'answer', 
+      ({g, context}) => `answering with ${context.type}` 
+    ],
   ],
 
   semantics: [
     [({objects, context}) => context.marker == 'list', async ({objects, context, api}) => {
       context.listing = api.getAllProducts()
       context.isResponse = true
+    }],
+    [({context}) => context.marker == 'answer', async ({objects, context, api}) => {
+      objects.listing = objects.listing || {}
+      objects.listing.type = context.type
     }],
   ],
 };
@@ -90,9 +126,7 @@ key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
 // shirts less than 10 dollars
 // shirts not more than 10 dollars
 // pants that are exactly $10
-
-config = new entodicton.Config(config).add(currencyKM)
-console.dir(config.config)
+config = new entodicton.Config(config).add(currencyKM).add(helpKM)
 config.api = api
 config.initializer( ({objects, api}) => {
   api.getTypes().forEach( (type) => {
@@ -102,6 +136,10 @@ config.initializer( ({objects, api}) => {
       words[type].push(def)
     } else {
       words[type] = [def]
+    }
+    objects.listing = { 
+      type: 'tables',
+      columns: ['name'],
     }
   })
   config.get('generators').push( api.productGenerator )
@@ -135,8 +173,11 @@ entodicton.knowledgeModule( {
         }
         console.log(responses.trace);
         console.log('objects', JSON.stringify(config.get("objects"), null, 2))
-        console.log(responses.generated);
         console.log(JSON.stringify(responses.results, null, 2));
+        console.log('Responses ------------------------\n')
+        for (let response of responses.generated[0]) {
+          console.log(response)
+        }
 //        return promise;
       })
       .catch( (error) => {
