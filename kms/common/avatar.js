fdd97993b47bbed99bc80d49a28e697f291ac365
Greg McClement <greg.mcclement@gmail.com>
2021-06-13 07:18:00 -0700

    remove debug:

--- kms/common/avatar.js
+++ kms/common/avatar.js
@@ -96,8 +96,8 @@ let config = {
 
 url = "http://184.67.27.82"
 key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
-url = "http://localhost:3000"
-key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
+//url = "http://localhost:3000"
+//key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
 config = new entodicton.Config(config)
 config.add(dialogues)
 config.api = api
--- kms/common/dialogues.js
+++ kms/common/dialogues.js
@@ -123,8 +123,8 @@ let config = {
 
 url = "http://184.67.27.82"
 key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
-url = "http://localhost:3000"
-key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
+//url = "http://localhost:3000"
+//key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
 config = new entodicton.Config(config)
 config.api = api
 
