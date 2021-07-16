36b83b49c35c4a3310a1efd96e8599039454399c
Greg McClement <greg.mcclement@gmail.com>
2021-07-15 18:24:25 -0700

    fix ekms

--- kms/main.js
+++ kms/main.js
@@ -1,3 +1,21 @@
-const store = require('./common/store');
+const avatar = require('./common/avatar');
+const characters = require('./common/characters');
+const currency = require('./common/currency');
+const dialogues = require('./common/dialogues');
+const help = require('./common/help');
+const javascript = require('./common/javascript');
+const numbers = require('./common/numbers');
+const reports = require('./common/reports');
+const time = require('./common/time');
 
-module.exports = { store }
+module.exports = { 
+  avatar,
+  characters,
+  currency,
+  dialogues,
+  help,
+  javascript,
+  numbers,
+  reports,
+  time,
+}
