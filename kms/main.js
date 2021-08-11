60bba9afa20361e33529f79a282c704f2cf110b9
Greg McClement <greg.mcclement@gmail.com>
2021-08-10 21:35:19 -0700

    more fix

--- kms/main.js
+++ kms/main.js
@@ -5,7 +5,9 @@ const dialogues = require('./common/dialogues');
 const help = require('./common/help');
 const javascript = require('./common/javascript');
 const numbers = require('./common/numbers');
+const properties = require('./common/properties');
 const reports = require('./common/reports');
+const tell = require('./common/tell');
 const time = require('./common/time');
 
 module.exports = { 
