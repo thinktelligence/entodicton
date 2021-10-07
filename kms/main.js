e2cddc059118c63c43641b2fa63bf04ef50545da
Greg McClement <greg.mcclement@gmail.com>
2021-10-06 18:35:52 -0700

    missed tis bit

--- kms/main.js
+++ kms/main.js
@@ -5,7 +5,6 @@ const currency = require('./common/currency');
 const dialogues = require('./common/dialogues');
 const help = require('./common/help');
 const javascript = require('./common/javascript');
-const motivations = require('./common/motivations');
 const numbers = require('./common/numbers');
 const people = require('./common/people');
 const properties = require('./common/properties');
@@ -22,7 +21,6 @@ module.exports = {
   dialogues,
   help,
   javascript,
-  motivations,
   numbers,
   properties,
   reports,
