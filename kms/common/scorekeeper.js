da059041d47d35bef877ad2650405b838137e7ba
Greg McClement <greg.mcclement@gmail.com>
2021-10-07 20:37:11 -0700

    fix bug

--- kms/common/scorekeeper.js
+++ kms/common/scorekeeper.js
@@ -74,6 +74,7 @@ let config = {
   },
 
   priorities: [
+    [['is', 0], ['whose', 0], ['start', 0], ['point', 0]],
     [['is', 0], ['whose', 0], ['turn', 0]],
     [['a', 0], ['start', 0], ['new', 0]],
     [['start', 0], ['number', 0], ['point', 0], ['a', 0]],
