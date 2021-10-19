1e6e7d840d1889228d3cda45601adcae6fbeb763
Greg McClement <greg.mcclement@gmail.com>
2021-10-18 19:52:48 -0700

    fix

--- kms/common/scorekeeper.js
+++ kms/common/scorekeeper.js
@@ -86,6 +86,7 @@ let config = {
   },
 
   priorities: [
+    [['a', 0], ['game', 0], ['is', 0], ['it', 0], ['whose', 0], ['number', 0], ['point', 0], ['start', 0], ['turn', 0], ['new', 0]],
     [['a', 0], ['is', 0], ['it', 0], ['whose', 0], ['number', 0], ['point', 0], ['start', 0], ['new', 0]],
     [['whose', 0], ['is', 0], ['start', 0], ['a', 0]],
     [['is', 0], ['next', 0], ['scored', 0], ['scored', 0], ['point', 0]],
