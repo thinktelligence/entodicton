9618e8114fc67a90091af1d92bee1474e3b9d61c
Greg McClement <greg.mcclement@gmail.com>
2021-12-10 14:43:16 -0800

    new template model

--- kms/common/animals.js
+++ kms/common/animals.js
@@ -1,11 +1,29 @@
 const entodicton = require('entodicton')
-const base = require('./hierarchy').copy()
+const hierarchy = require('./hierarchy').copy()
 const animals_tests = require('./animals.test.json')
-const animals_instance = require('./hierarchy.animals.instance.json')
+const animals_instance = require('./animals.instance.json')
+
+const template ={
+  "queries": [
+    "humans are mammels",
+    "mammels are animals",
+    "mammels have ears",
+    "mammels dont have wings",
+    "animals have skin",
+    "birds are animals",
+    "birds have wings",
+    "felines are animals",
+    "cats are felines",
+    "canines are animals",
+    "dogs are canines",
+    "bats are mammels",
+    "bats have wings"
+  ],
+}
 
 const config = new entodicton.Config({ name: 'animals' })
-config.add(base)
-config.load(animals_instance)
+config.add(hierarchy)
+config.load(template, animals_instance)
 
 entodicton.knowledgeModule( {
     module,
