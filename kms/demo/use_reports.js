6a1fc12936f12d88db64937e4135635bc8241eec
Greg McClement <greg.mcclement@gmail.com>
2023-09-03 08:01:52 -0700

    fix up readme wip

--- kms/demo/use_reports.js
+++ kms/demo/use_reports.js
@@ -1,4 +1,4 @@
-const { store } = require('ekms')
+const { reports } = require('ekms')
 
 const testData = {
   types: [ 'tanks', 'planes' ],
@@ -23,13 +23,9 @@ const interface = {
   productGenerator: [({context}) => context.marker == 'product' && context.isInstance, ({g, context}) => `${context.name}`]
 };
 
-store.interface = interface
+reports.interface = interface
 
-url = "http://184.67.27.82"
-key = "6804954f-e56d-471f-bbb8-08e3c54d9321"
-store.server(url, key)
-store.process('list the products').then( (response) => {
-  //console.log(JSON.stringify(response, null, 2))
+reports.process('list the products').then( (response) => {
   for (r of response.generated[0]) {
     console.log(r)
   }
