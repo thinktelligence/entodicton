8fb90fd5a2aaeb660853af9f00bd0e6b8098da9c
Greg McClement <greg.mcclement@gmail.com>
2023-09-17 11:07:14 -0700

    version 7.3.2-beta.0

--- kms/website/config_earn.js
+++ kms/website/config_earn.js
@@ -2,25 +2,34 @@
 
 module.exports = 
 {
-  "flatten": [
-    "conj",
-  ],
-  "implicits": [
-    "language",
-  ],
   "floaters": [
     "isQuery",
   ],
-  "bridges": [
-    {"bridge": "{ ...next(operator) }", "id": "week", "level": 0},
-    {"bridge": "{ ...next(operator) }", "id": "dollarConcept", "level": 0},
-    {"bridge": "{ ...next(operator) }", "id": "personConcept", "level": 0},
-    {"bridge": "{ marker: 'dollarConcept', units: 'dollars', amount: before.value, duration: 'week' }", "id": "every", "level": 0},
-    {"bridge": "{ marker: 'earn', units: 'dollars', amount: after.amount, who: before.id, period: after.duration }", "id": "earn", "level": 0},
-    {"bridge": "{ marker: 'worked', who: before.id, duration: after.number, units: after.marker }", "id": "worked", "level": 0},
+  "associations": {
+    "negative": [],
+    "positive": [],
+  },
+  "words": {
+    "dollars": [{"id": "dollarConcept", "initial": {"language": "english"}}],
+    "week": [{"id": "week", "initial": {"language": "english"}}],
+    "per": [{"id": "every"}],
+    "joe": [{"id": "personConcept", "initial": {"id": "joe"}}],
+    "sally": [{"id": "personConcept", "initial": {"id": "sally"}}],
+  },
+  "priorities": [
+    [["earn", 0], ["worked", 0], ["every", 0], ["query", 0]],
+    [["earn", 0], ["worked", 0], ["query", 0], ["count", 0]],
+    [["earn", 0], ["every", 0], ["worked", 0]],
   ],
-  "utterances": [
-    "joe earns 10 dollars every week sally earns 25 dollars per week sally worked 10 weeks joe worked 15 weeks joe earns what sally earns what",
+  "hierarchy": [
+  ],
+  "flatten": [
+    "conj",
+  ],
+  "operators": [
+    "(([personConcept]) [earn|earns] ((<count> ([dollarConcept])) [every] ([week])))",
+    "(([personConcept]) [earn] ([query|what]))",
+    "(([personConcept]) [worked] (<count> ([week|weeks])))",
   ],
   "generators": [
     [({context}) => context.marker == 'week' && context.duration == 1, ({g, context}) => `${context.duration} week`],
@@ -28,6 +37,20 @@ module.exports =
     [({context}) => context.marker == 'earn', ({g, context}) => `${g(context.who)} earns ${g(context.amount)} ${g(context.units)} per ${context.period}`],
     [({context}) => context.marker == 'worked', ({g, context}) => `${g(context.who)} worked ${ g({ marker: context.units, duration: context.duration}) }`],
     [({context}) => context.marker == 'response', ({g, context}) => `${context.who} earned ${context.earnings} ${context.units}`],
+  ],
+  "bridges": [
+    {"id": "week", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "dollarConcept", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "personConcept", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "every", "bridge": "{ marker: 'dollarConcept', units: 'dollars', amount: before.value, duration: 'week' }", "level": 0},
+    {"id": "earn", "bridge": "{ marker: 'earn', units: 'dollars', amount: after.amount, who: before.id, period: after.duration }", "level": 0},
+    {"id": "worked", "bridge": "{ marker: 'worked', who: before.id, duration: after.number, units: after.marker }", "level": 0},
+  ],
+  "implicits": [
+    "language",
+  ],
+  "utterances": [
+    "joe earns 10 dollars every week sally earns 25 dollars per week sally worked 10 weeks joe worked 15 weeks joe earns what sally earns what",
   ],
   "semantics": [
     [({objects, context}) => context.marker == 'earn' && context.isQuery, ({objects, context}) => { 
@@ -60,28 +83,5 @@ module.exports =
     Object.assign(context, object)
     delete context.pullFromContext
      }],
-  ],
-  "operators": [
-    "(([personConcept]) [earn|earns] ((<count> ([dollarConcept])) [every] ([week])))",
-    "(([personConcept]) [earn] ([query|what]))",
-    "(([personConcept]) [worked] (<count> ([week|weeks])))",
-  ],
-  "hierarchy": [
-  ],
-  "associations": {
-    "positive": [],
-    "negative": [],
-  },
-  "words": {
-    "dollars": [{"id": "dollarConcept", "initial": {"language": "english"}}],
-    "per": [{"id": "every"}],
-    "week": [{"id": "week", "initial": {"language": "english"}}],
-    "joe": [{"id": "personConcept", "initial": {"id": "joe"}}],
-    "sally": [{"id": "personConcept", "initial": {"id": "sally"}}],
-  },
-  "priorities": [
-    [["earn", 0], ["worked", 0], ["every", 0], ["query", 0]],
-    [["earn", 0], ["worked", 0], ["query", 0], ["count", 0]],
-    [["earn", 0], ["every", 0], ["worked", 0]],
   ],
 };
--- kms/website/config_food.js
+++ kms/website/config_food.js
@@ -2,24 +2,53 @@
 
 module.exports = 
 {
-  "flatten": [
-    "conj",
-  ],
-  "implicits": [
-    "language",
-  ],
   "floaters": [
     "isQuery",
   ],
+  "associations": {
+    "negative": [],
+    "positive": [],
+  },
+  "words": {
+    "cheeseburger": [{"id": "food", "initial": {"name": "cheeseburger"}}],
+    "cheeseburgers": [{"id": "food", "initial": {"name": "cheeseburger"}}],
+    "fries": [{"id": "food", "initial": {"name": "fries", "number": "many"}}],
+  },
+  "priorities": [
+    [["conj", 0], ["plus", 0]],
+  ],
+  "hierarchy": [
+  ],
+  "flatten": [
+    "conj",
+  ],
+  "operators": [
+    "(([i]) [wantMcDonalds|want] ([food]) ([fromM|from] ([mcdonalds])))",
+    "(([i]) [wantWhitespot|want] ([food]) ([fromW|from] ([whitespot])))",
+    "(<count> ([food]))",
+    "(<aEnglish> ([food]))",
+    "(([food]) [conj] ([food]))",
+  ],
+  "generators": [
+    [({context}) => context.marker == 'wantWhitespot', ({g, context}) => `order for ${g(context.items)} from ${g(context.store)}`],
+    [({context}) => context.marker == 'wantMcDonalds', ({g, context}) => `order for ${g(context.items)} from ${g(context.store)}`],
+    [({context}) => context.marker == 'food' && context.number > 0, ({g, context}) => `${g(context.number)} ${g(context.name)}`],
+    [({context}) => context.marker == 'food' && !('number' in context), ({g, context}) => `${g(context.name)}`],
+    [({context}) => context.marker == 'whitespot', ({g, context}) => 'Whitespot'],
+    [({context}) => context.marker == 'mcdonalds', ({g, context}) => 'McDonalds'],
+  ],
   "bridges": [
-    {"bridge": "{ ...next(operator) }", "id": "food", "level": 0},
-    {"bridge": "{ ...next(operator) }", "id": "mcdonalds", "level": 0},
-    {"bridge": "{ ...next(operator) }", "id": "whitespot", "level": 0},
-    {"bridge": "{ ...next(operator) }", "id": "i", "level": 0},
-    {"bridge": "{ ...next(operator), action: 'order', items: after[0], store: after[1].from }", "id": "wantMcDonalds", "level": 0},
-    {"bridge": "{ ...next(operator), action: 'order', items: after[0], store: after[1].from }", "id": "wantWhitespot", "level": 0},
-    {"bridge": "{ ...next(operator), from: after[0] }", "id": "fromM", "level": 0},
-    {"bridge": "{ ...next(operator), from: after[0] }", "id": "fromW", "level": 0},
+    {"id": "food", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "mcdonalds", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "whitespot", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "i", "bridge": "{ ...next(operator) }", "level": 0},
+    {"id": "wantMcDonalds", "bridge": "{ ...next(operator), action: 'order', items: after[0], store: after[1].from }", "level": 0},
+    {"id": "wantWhitespot", "bridge": "{ ...next(operator), action: 'order', items: after[0], store: after[1].from }", "level": 0},
+    {"id": "fromM", "bridge": "{ ...next(operator), from: after[0] }", "level": 0},
+    {"id": "fromW", "bridge": "{ ...next(operator), from: after[0] }", "level": 0},
+  ],
+  "implicits": [
+    "language",
   ],
   "utterances": [
     "i want 2 fries and a cheeseburger from mcdonalds",
@@ -33,35 +62,6 @@ module.exports =
     "i want fries from mcdonalds",
     "i want cheeseburger and fries from whitespot",
   ],
-  "generators": [
-    [({context}) => context.marker == 'wantWhitespot', ({g, context}) => `order for ${g(context.items)} from ${g(context.store)}`],
-    [({context}) => context.marker == 'wantMcDonalds', ({g, context}) => `order for ${g(context.items)} from ${g(context.store)}`],
-    [({context}) => context.marker == 'food' && context.number > 0, ({g, context}) => `${g(context.number)} ${g(context.name)}`],
-    [({context}) => context.marker == 'food' && !('number' in context), ({g, context}) => `${g(context.name)}`],
-    [({context}) => context.marker == 'whitespot', ({g, context}) => 'Whitespot'],
-    [({context}) => context.marker == 'mcdonalds', ({g, context}) => 'McDonalds'],
-  ],
   "semantics": [
-  ],
-  "operators": [
-    "(([i]) [wantMcDonalds|want] ([food]) ([fromM|from] ([mcdonalds])))",
-    "(([i]) [wantWhitespot|want] ([food]) ([fromW|from] ([whitespot])))",
-    "(<count> ([food]))",
-    "(<aEnglish> ([food]))",
-    "(([food]) [conj] ([food]))",
-  ],
-  "hierarchy": [
-  ],
-  "associations": {
-    "positive": [],
-    "negative": [],
-  },
-  "words": {
-    "fries": [{"id": "food", "initial": {"name": "fries", "number": "many"}}],
-    "cheeseburgers": [{"id": "food", "initial": {"name": "cheeseburger"}}],
-    "cheeseburger": [{"id": "food", "initial": {"name": "cheeseburger"}}],
-  },
-  "priorities": [
-    [["conj", 0], ["plus", 0]],
   ],
 };
